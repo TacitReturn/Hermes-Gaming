37fea4908c2d83218028002237b97c3d5120188a
Glenn Rudge <glennrudge@outlook.com>
2020-09-13 23:01:20 -0400

    Added tailwind css to project, created layout directory for app, and an index view

---
+++ resources/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Hermes Game Aggragator</title>
+</head>
+<body class="bg-gray-900 text-white">
+<header class="border-b border-gray-800">
+    <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+        <div class="flex items-center">
+            <a href="/">
+                <img src="" alt="hermes" class="w-32 flex-none">
+            </a>
+            <ul class="flex">
+                <li><a href="" class="hover:text-gray-400">Games</a></li>
+                <li><a href="" class="hover:text-gray-400">Reviews</a></li>
+                <li><a href="" class="hover:text-gray-400">Coming Soon</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+</body>
+</html>
