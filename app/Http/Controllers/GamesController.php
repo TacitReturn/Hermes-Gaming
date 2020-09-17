d5a6f83960d8ecb83087d8148c055e5fc888de02
Glenn Rudge <glennrudge@outlook.com>
2020-09-16 21:27:01 -0400

    Created LiveWire ComingSoon class and component

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -15,77 +15,7 @@ class GamesController extends Controller
      */
     public function index()
     {
-        $before = Carbon::now()->subMonths(6)->timestamp;
-        $after = Carbon::now()->addMonths(6)->timestamp;
-        $current = Carbon::now()->timestamp;
-        $afterFourMonths = Carbon::now()->addMonths(4)->timestamp;
-//
-//        $popularGames = Http::withHeaders(config('services.igdb'))
-//            ->withOptions([
-//                'body' => "
-//                          fields name, cover.url, first_release_date, popularity, platforms.abbreviation, rating;
-//                          where platforms = (48,49,130,6)
-//                          & ( first_release_date >= {$before}
-//                           & first_release_date < {$after});
-//                          sort popularity desc;
-//                          limit 12;
-//                          "
-//            ])->get('https://api-v3.igdb.com/games/')
-//            ->json();
-
-//        dump($popularGames);
-
-//        $recentlyReviewed = Http::withHeaders(config('services.igdb'))
-//            ->withOptions([
-//                'body' => "
-//                          fields name, cover.url, first_release_date, popularity, platforms.abbreviation, rating, rating_count, summary;
-//                          where platforms = (48,49,130,6)
-//                          & ( first_release_date >= {$before}
-//                          & first_release_date < {$current}
-//                          & rating_count > 5);
-//                           sort popularity desc;
-//                           limit 5;
-//                          "
-//            ])->get('https://api-v3.igdb.com/games/')
-//            ->json();
-
-//        dump($recentlyReviewed);
-
-//        $mostAnticipated = Http::withHeaders(config('services.igdb'))
-//            ->withOptions([
-//                'body' => "
-//                          fields name, cover.url, first_release_date, popularity, platforms.abbreviation, rating, rating_count, summary;
-//                          where platforms = (48,49,130,6)
-//                          & ( first_release_date >= {$current}
-//                          & first_release_date < {$afterFourMonths});
-//                          & rating_count > 5);
-//                           sort popularity desc;
-//                           limit 4;
-//                          "
-//            ])->get('https://api-v3.igdb.com/games/')
-//            ->json();
-
-//        dump($mostAnticipated);
-
-        $comingSoon = Http::withHeaders(config('services.igdb'))
-            ->withOptions([
-                'body' => "
-                          fields name, cover.url, first_release_date, popularity, platforms.abbreviation, rating, rating_count, summary;
-                          where platforms = (48,49,130,6)
-                          & ( first_release_date >= {$current}
-                          & popularity > 5);
-                          sort first_release_date asc;
-                          limit 4;
-                          "
-            ])->get('https://api-v3.igdb.com/games/')
-            ->json();
-
-//        dump($comingSoon);
-
-        return view('index', [
-            'comingSoon' => $comingSoon
-
-        ]);
+        return view('index');
     }
 
     /**
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,34 +3,17 @@
 @section('content')
     <div class="container mx-auto px-4">
         <h2 class="text-gray-500 uppercase tracking-wide font-semibold">Popular Game</h2>
-        <livewire:popular-games>
+        <livewire:popular-games />
             <div class="flex flex-col lg:flex-row m-10">
                 <div class="recently-reviewed w-full lg:w-3/4 mr-0 w-3/4 lg:mr-32">
                     <h2 class="text-gray-500 tracking-wide uppercase font-semibold">Recently Reviewed</h2>
-                    <livewire:recently-reviewed>
+                    <livewire:recently-reviewed />
                 </div>
                 <div class="mx-auto  most-anticipated lg:w-1/4 mt-12 lg:mt-0">
                     <h2 class="text-gray-500 tracking-wide uppercase font-semibold">Most Anticipated</h2>
-                    <livewire:most-anticipated>
+                    <livewire:most-anticipated />
                         <h2 class="text-gray-500 tracking-wide uppercase font-semibold mt-5">Coming Soon</h2>
-                        <div class="most-anticipated container space-y-10 mt-8">
-                            @foreach($comingSoon as $game)
-                                <div class="game flex">
-                                    <a href="">
-                                        <img
-                                            src="{{ (Str::replaceFirst('thumb', 'cover_small', $game['cover']['url']))}}"
-                                            class="hover:opacity-75 transition ease-in-out duration-150"
-                                            alt="Game image"/>
-                                    </a>
-                                    <div class="ml-5">
-                                        <a href="" class="hover:text-gray-400">{{ $game['name'] }}</a>
-                                        <p class="text-white text-sm mt-1">
-                                            {{ \Carbon\Carbon::parse($game['first_release_date'])->format('M d, Y') }}
-                                        </p>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
+                    <livewire:coming-soon />
                 </div>
             </div>
     </div>
